4547c6645c523ae1b07ed11ac4c8416acf77ea44
Ar <Ar>
2020-12-09 10:58:30 -0500

    init git

---
+++ InventoryApp/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace InventoryApp.Controllers
+
+{   [RoutePrefix("products2")]
+    //[Route({"action=index"})]
+    public class ProductsController : Controller
+    {
+
+        //route using route table
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //route using route attributes
+
+        [Route("~/collections2")]
+        public ActionResult Collections()
+        {
+            return View("Collections");
+        }
+
+
+        [Route("produce")]
+        public ActionResult Produce()
+        {
+            return RedirectToAction("Produce","Home");
+        }
+
+
+
+    }
+}
---
+++ InventoryApp/Controllers/ProfileController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventoryApp.Controllers
+{
+    public class ProfileController : Controller
+    {
+
+        [Route("profile/{username}")]
+        public ActionResult Viewprofile(string username)
+        {
+            ViewData["name"] = username;
+            return View("profile");
+        }
+    }
+}
